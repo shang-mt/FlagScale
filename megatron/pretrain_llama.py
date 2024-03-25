869a74a52f54120ca45c8d3fbc4ea41654440151
Xiaokang Shang <xiaokang.shang@mthreads.com>
2024-03-25 22:42:16 +0800

    new llama

--- megatron/pretrain_llama.py
+++ megatron/pretrain_llama.py
@@ -1,36 +1,37 @@
 # Copyright (c) 2023, NVIDIA CORPORATION.  All rights reserved.
-"""Pretrain LLaMA."""
+
+"""Pretrain GPT"""
 
 import os
+import sys
+
+# Add mup package to sys.path
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), 'mup/'))
+
 import torch
-from torch import Tensor
 from functools import partial
 from megatron import get_args
 from megatron import print_rank_0
 from megatron import get_timers
 from megatron import get_tokenizer
-from megatron.core import mpu, tensor_parallel
+from megatron.core import tensor_parallel
 from megatron.core.enums import ModelType
-from megatron.core.datasets.blended_megatron_dataset_builder import BlendedMegatronDatasetBuilder
-from megatron.core.datasets.gpt_dataset import GPTDatasetConfig
-from megatron.core.datasets.gpt_dataset import GPTDataset
-from megatron.model import LLaMAModel
+from megatron.data.gpt_dataset import build_train_valid_test_datasets
+from megatron.model import GPTModel
 from megatron.training import pretrain
-from megatron.utils import (
-    get_ltor_masks_and_position_ids,
-    get_batch_on_this_cp_rank,
-    average_losses_across_data_parallel_group
-)
+from megatron.utils import get_ltor_masks_and_position_ids
+from megatron.utils import average_losses_across_data_parallel_group
 from megatron.arguments import core_transformer_config_from_args
 
 
 def model_provider(pre_process=True, post_process=True):
     """Build the model."""
-    args = get_args()
-    config = core_transformer_config_from_args(args)
-    print_rank_0('building LLaMA model ...')
-    model = LLaMAModel(
-        config=config,
+
+    print_rank_0('building GPT model ...')
+    config = core_transformer_config_from_args(get_args())
+    model = GPTModel(
+        config,
         num_tokentypes=0,
         parallel_output=True,
         pre_process=pre_process,
@@ -40,12 +41,7 @@ def model_provider(pre_process=True, post_process=True):
 
 
 def get_batch(data_iterator):
-    """Generate a batch."""
-
-    # TODO: this is pretty hacky, find a better way
-    if (not mpu.is_pipeline_first_stage()) and (not mpu.is_pipeline_last_stage()):
-        return None, None, None, None, None
-
+    """Generate a batch"""
     args = get_args()
     tokenizer = get_tokenizer()
 
@@ -58,6 +54,17 @@ def get_batch(data_iterator):
         data = next(data_iterator)
     else:
         data = None
+
+    # Extra items
+    if args.return_doc_ids and data is not None:
+        _key = 'doc_ids'
+        if _key in data.keys():
+            keys.append(_key)
+        _key = 'dataset_idx'
+        if _key in data.keys():
+            data[_key] = data[_key].long()
+            keys.append(_key)
+
     data_b = tensor_parallel.broadcast_data(keys, data, datatype)
 
     # Unpack.
@@ -73,55 +80,32 @@ def get_batch(data_iterator):
         args.reset_attention_mask,
         args.eod_mask_loss)
 
-    batch = {
-        'tokens': tokens,
-        'labels': labels,
-        'loss_mask': loss_mask,
-        'attention_mask': attention_mask,
-        'position_ids': position_ids
-    }
-    # slice batch along sequence dimension for context parallelism
-    batch = get_batch_on_this_cp_rank(batch)
+    if args.data_searching_save is not None and args.return_doc_ids:
+        doc_ids = None
+        _key = 'doc_ids'
+        if _key in keys:
+            doc_ids = data_b['doc_ids'].long()
+        dataset_idx = None
+        _key = 'dataset_idx'
+        if _key in keys:
+            dataset_idx = data_b['dataset_idx'].long()
+        return tokens, labels, loss_mask, attention_mask, position_ids, dataset_idx
+    else:
+        return tokens, labels, loss_mask, attention_mask, position_ids
 
-    return batch.values()
-
-def loss_func(loss_mask: Tensor, output_tensor: Tensor):
-    """Loss function.
-    Args:
-        loss_mask (Tensor): Used to mask out some portions of the loss
-        output_tensor (Tensor): The tensor with the losses
-    """
-    args = get_args()
-
+def loss_func(loss_mask, output_tensor):
     losses = output_tensor.float()
     loss_mask = loss_mask.view(-1).float()
-    if args.context_parallel_size > 1:
-        loss = torch.cat([torch.sum(losses.view(-1) * loss_mask).view(1), loss_mask.sum().view(1)])
-        torch.distributed.all_reduce(loss, group=mpu.get_context_parallel_group())
-        loss = loss[0] / loss[1]
-    else:
-        loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()
-
-    # Check individual rank losses are not NaN prior to DP all-reduce.
-    if args.check_for_nan_in_loss_and_grad:
-        global_rank = torch.distributed.get_rank()
-        assert not loss.isnan(), (
-            f'Rank {global_rank}: found NaN in local forward loss calculation. '
-            f'Device: {torch.musa.current_device()}, node: {os.uname()[1]}'
-        )
+    loss = torch.sum(losses.view(-1) * loss_mask) / loss_mask.sum()
 
     # Reduce loss for logging.
     averaged_loss = average_losses_across_data_parallel_group([loss])
 
-    return loss * args.context_parallel_size, {'lm loss': averaged_loss[0]}
+    return loss, {'lm loss': averaged_loss[0]}
 
 
-def forward_step(data_iterator, model: LLaMAModel):
-    """Forward training step.
-    Args:
-        data_iterator : Input data iterator
-        model (LLaMAModel): The LLaMA Model
-    """
+def forward_step(data_iterator, model):
+    """Forward step."""
     args = get_args()
     timers = get_timers()
 
@@ -137,50 +121,35 @@ def forward_step(data_iterator, model: LLaMAModel):
     return output_tensor, partial(loss_func, loss_mask)
 
 
-def is_dataset_built_on_rank():
-    return (mpu.is_pipeline_first_stage() or mpu.is_pipeline_last_stage()) and mpu.get_tensor_model_parallel_rank() == 0
-
-
-def core_llama_dataset_config_from_args(args):
-    return GPTDatasetConfig(
-        is_built_on_rank=is_dataset_built_on_rank,
-        random_seed=args.seed,
-        sequence_length=args.seq_length,
-        blend=args.data_path,
-        blend_per_split=[args.train_data_path, args.valid_data_path, args.test_data_path],
-        split=args.split,
-        path_to_cache=args.data_cache_path,
-    )
-
-
 def train_valid_test_datasets_provider(train_val_test_num_samples):
-    """Build the train test and validation datasets.
-    Args:
-        train_val_test_num_samples : A list containing the number of samples in train test and validation.
-    """
+    """Build train, valid, and test datasets."""
     args = get_args()
 
-    print_rank_0("> building train, validation, and test datasets for GPT ...")
-
-    train_ds, valid_ds, test_ds = BlendedMegatronDatasetBuilder(
-        GPTDataset,
-        train_val_test_num_samples,
-        core_llama_dataset_config_from_args(args)
-    ).build()
-
-    print_rank_0("> finished creating LLaMA datasets ...")
+    print_rank_0('> building train, validation, and test datasets '
+                 'for GPT ...')
+    train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
+        data_prefix=args.data_path,
+        data_impl=args.data_impl,
+        splits_string=args.split,
+        train_valid_test_num_samples=train_val_test_num_samples,
+        seq_length=args.seq_length,
+        seed=args.seed,
+        skip_warmup=(not args.mmap_warmup),
+        train_data_prefix=args.train_data_path,
+        valid_data_prefix=args.valid_data_path,
+        test_data_prefix=args.test_data_path,
+        return_doc_ids=args.return_doc_ids,
+        data_cache_path=args.data_cache_path)
+    print_rank_0("> finished creating GPT datasets ...")
 
     return train_ds, valid_ds, test_ds
 
 
 if __name__ == "__main__":
 
-    # Temporary for transition to core datasets
-    train_valid_test_datasets_provider.is_distributed = True
-
     pretrain(train_valid_test_datasets_provider,
              model_provider,
              ModelType.encoder_or_decoder,
              forward_step,
-             args_defaults={'tokenizer_type': 'Llama2Tokenizer'},
+             args_defaults={'tokenizer_type': 'GPT2BPETokenizer'},
              get_batch_fn=get_batch)
